d6abb4574211b181e59b38c2b76835bddc6309ac
shainasahu <145936084+shainasahu@users.noreply.github.com>
2025-10-23 20:00:46 -0400

    modified bonsai ui

--- ui/tictactoe_ui.ml
+++ ui/tictactoe_ui.ml
@@ -4,120 +4,130 @@ open Hw2_tictactoe_logic
 open Virtual_dom
 open! Bonsai.Let_syntax
 
-let lookup_cell (game_state : Game_state.t) ~row ~column =
-  Map.find game_state.board { row; column }
-;;
-
-let viewbox = Vdom.Attr.create "viewBox" "0 0 100 100"
-
-let o_mark =
-  Vdom.Node.inner_html_svg
-    ~tag:"svg"
-    ~attrs:[ viewbox ]
-    ~this_html_is_sanitized_and_is_totally_safe_trust_me:
-      "<circle cx='50' cy='50' r='27' stroke='blue' stroke-width='5' fill='white' />"
-    ()
-;;
-
-let x_mark =
-  Vdom.Node.inner_html_svg
-    ~tag:"svg"
-    ~attrs:[ viewbox ]
-    ~this_html_is_sanitized_and_is_totally_safe_trust_me:
-      "<line x1='25' y1='25' x2='75' y2='75' stroke='red' stroke-width='5' /> <line \
-       x1='25' y1='75' x2='75' y2='25' stroke='red' stroke-width='5' />"
-    ()
-;;
-
-let tic_tac_toe_board ~(game_state : Game_state.t) ~set_game_state =
-  let is_game_over = Decision.is_game_over game_state.decision in
-  let render_cell ~row ~column =
-    let cell_value = lookup_cell game_state ~row ~column in
-    let mark, maybe_clickable_attr =
-      match cell_value with
-      | Some X -> x_mark, Vdom.Attr.empty
-      | Some O -> o_mark, Vdom.Attr.empty
-      | None when is_game_over -> Vdom.Node.none, Vdom.Attr.empty
-      | None ->
-        let attr =
-          Vdom.Attr.(
-            class_ "box-shadow-with-hover-effect"
-            @ on_click (fun _ ->
-              match Game_state.make_move game_state { row; column } with
-              | Error _ -> raise_s [%message "Invalid move" (row : int) (column : int)]
-              | Ok new_game_state -> set_game_state new_game_state))
+let render_card ~card ~clickable ~on_click =
+  let card_content, is_face_down =
+    match card with
+    | `Face_down -> ("", true)
+    | `Card card_obj -> 
+        let rank_str =
+          match card_obj.Card.rank with
+          | Rank.Two -> "2" | Rank.Three -> "3" | Rank.Four -> "4" | Rank.Five -> "5"
+          | Rank.Six -> "6" | Rank.Seven -> "7" | Rank.Eight -> "8" | Rank.Nine -> "9"
+          | Rank.Ten -> "10" | Rank.Jack -> "J" | Rank.Queen -> "Q" | Rank.King -> "K" | Rank.Ace -> "A"
         in
-        Vdom.Node.none, attr
-    in
-    let attrs =
-      [ Vdom.Attr.class_ "column"
-      ; Vdom.Attr.style
-          Css_gen.(
-            left
-              (`Percent
-                  (Percent.of_percentage
-                     (Int.to_float column *. 100.0 /. Int.to_float game_state.columns)))
-            @> width
-                 (`Percent
-                     (Percent.of_percentage (100.0 /. Int.to_float game_state.columns))))
-      ; maybe_clickable_attr
-      ]
-    in
-    let attrs =
-      attrs
-      @ if row < game_state.rows - 1 then [ Vdom.Attr.class_ "border_bottom" ] else []
-    in
-    let attrs =
-      attrs
-      @
-      if column < game_state.columns - 1 then [ Vdom.Attr.class_ "border_right" ] else []
-    in
-    let should_slowly_appear =
-      match game_state.last_move with
-      | None -> []
-      | Some last_move ->
-        if Move.equal last_move { row; column }
-        then [ Vdom.Attr.class_ "slowly_appear" ]
-        else []
-    in
-    Vdom.Node.div
-      ~attrs
-      [ Vdom.Node.div
-          ~attrs:(should_slowly_appear @ [ Vdom.Attr.class_ "max_size" ])
-          [ mark ]
-      ]
+        let suit_str =
+          match card_obj.Card.suit with
+          | Suit.Hearts -> "♥" | Suit.Diamonds -> "♦" | Suit.Clubs -> "♣" | Suit.Spades -> "♠"
+        in
+        (rank_str ^ suit_str, false)
+  in
+  let attrs =
+    let base = [ Vdom.Attr.class_ "card" ] in
+    let base = if is_face_down then base @ [ Vdom.Attr.class_ "face-down" ] else base in
+    if clickable
+    then base @ [ Vdom.Attr.on_click (fun _ -> on_click card) ]
+    else base
+  in
+  Vdom.Node.div ~attrs [ Vdom.Node.text card_content ]
+;;
+
+let render_hand ~cards ~on_card_click =
+  Vdom.Node.div ~attrs:[ Vdom.Attr.class_ "hand" ]
+    (List.mapi cards ~f:(fun _ c -> render_card ~card:c ~clickable:true ~on_click:on_card_click))
+;;
+
+let render_piles ~deck ~discard ~on_draw =
+  Vdom.Node.div ~attrs:[ Vdom.Attr.class_ "piles" ]
+    [ Vdom.Node.div
+        ~attrs:[ Vdom.Attr.class_ "deck"; Vdom.Attr.on_click (fun _ -> on_draw ()) ]
+        [ Vdom.Node.text (if List.is_empty deck then "EMPTY" else "DRAW") ];
+      Vdom.Node.div
+        ~attrs:[ Vdom.Attr.class_ "discard" ]
+        [ Vdom.Node.text
+            (match List.hd discard with
+            | None -> "EMPTY"
+            | Some ({ Card.rank; suit } : Card.t) ->
+              let rank_str =
+                match rank with
+                | Rank.Two -> "2" | Rank.Three -> "3" | Rank.Four -> "4" | Rank.Five -> "5"
+                | Rank.Six -> "6" | Rank.Seven -> "7" | Rank.Eight -> "8" | Rank.Nine -> "9"
+                | Rank.Ten -> "10" | Rank.Jack -> "J" | Rank.Queen -> "Q" | Rank.King -> "K" | Rank.Ace -> "A"
+              in
+              let suit_str =
+                match suit with
+                | Suit.Hearts -> "♥" | Suit.Diamonds -> "♦" | Suit.Clubs -> "♣" | Suit.Spades -> "♠"
+              in
+              rank_str ^ suit_str) ] ]
+;;
+
+let render_turn ~whose_turn =
+  let player_num =
+    match whose_turn with
+    | Player_kind.P1 -> 1
+    | Player_kind.P2 -> 2
   in
   Vdom.Node.div
-    ~attrs:[ Vdom.Attr.class_ "game" ]
-    (List.init game_state.rows ~f:(fun row ->
-       Vdom.Node.div
-         ~attrs:
-           [ Vdom.Attr.class_ "row"
-           ; Vdom.Attr.style
-               Css_gen.(
-                 top
-                   (`Percent
-                       (Percent.of_percentage
-                          (Int.to_float row *. 100.0 /. Int.to_float game_state.rows)))
-                 @> height
-                      (`Percent
-                          (Percent.of_percentage (100.0 /. Int.to_float game_state.rows))))
-           ]
-         (List.init game_state.columns ~f:(fun column -> render_cell ~row ~column))))
+    ~attrs:[ Vdom.Attr.class_ "turn-indicator" ]
+    [ Vdom.Node.text (Printf.sprintf "Player %d's turn!" player_num) ]
+;;
+
+let crazy_eights_board ~(game_state : Game_state.t) ~set_game_state =
+  let on_card_click card =
+    match card with
+    | `Face_down -> Vdom.Effect.Ignore  (* Can't click face-down cards *)
+    | `Card card_obj ->
+      match Game_state.make_move game_state (Move.Play [ card_obj ]) with
+      | Ok new_state -> set_game_state new_state
+      | Error _ -> Vdom.Effect.Ignore
+  in
+  
+  let on_draw () =
+    match Game_state.make_move game_state (Move.Draw_and_maybe_play None) with
+    | Ok new_state -> set_game_state new_state
+    | Error _ -> Vdom.Effect.Ignore
+  in
+
+  let current_player =
+    match game_state.decision with
+    | Decision.Winner _ -> Player_kind.P1
+    | Decision.In_progress { whose_turn; _ } -> whose_turn
+  in
+
+  let opponent_hands =
+    List.filter_map game_state.hands ~f:(fun (player, hand) ->
+        if Player_kind.equal player current_player
+        then None
+        else
+          let face_down_hand = List.map hand ~f:(fun _ -> `Face_down) in
+          Some (render_hand ~cards:face_down_hand ~on_card_click:(fun _ -> Vdom.Effect.Ignore)))
+  in
+
+  let current_hand =
+    let hand = List.Assoc.find_exn game_state.hands ~equal:Player_kind.equal current_player in
+    let hand_as_variant = List.map hand ~f:(fun card -> `Card card) in
+    render_hand ~cards:hand_as_variant ~on_card_click
+  in
+
+  let deck = game_state.deck in
+  let discard = game_state.discard_pile in
+
+  Vdom.Node.div ~attrs:[ Vdom.Attr.class_ "game" ]
+    (Vdom.Node.text "DEBUG: mounting!" ::
+      (List.concat [ opponent_hands
+                  ; [ render_piles ~deck ~discard ~on_draw ]
+                  ; [ current_hand ]
+                  ; [ render_turn ~whose_turn:current_player ] ])
+    )
 ;;
 
 let app =
-  let initial_state =
-    Game_state.create ~winning_sequence_length:3 ~rows:3 ~columns:3
-    |> Result.ok
-    |> Option.value_exn
-  in
+  let initial_state = Game_state.For_testing.sample_state in
   let%sub game_state, set_game_state =
     Bonsai.state ~default_model:initial_state (module Game_state)
   in
   let%arr game_state = game_state
   and set_game_state = set_game_state in
-  tic_tac_toe_board ~game_state ~set_game_state
+  crazy_eights_board ~game_state ~set_game_state
 ;;
 
 let () = Bonsai_web.Start.start app
